df77c41b4e584c87131f83af45a07faa5ccfc6d3
SARA3SAEED <Swaretoo@gmail.com>
2024-05-29 23:58:59 +0300

    1 commit

--- main.js
+++ main.js
@@ -1,119 +1,63 @@
 
+/*----------------------------------------------------------------*/
 
+        const imageNameInput = document.getElementById('imageName');
+        const imageUrlInput = document.getElementById('imageUrl');
+        const addImageBtn = document.getElementById('addImageBtn');
+        const imageContainer = document.getElementById('imageContainer');
 
+        const apiUrl = 'https://665736849f970b3b36c864e7.mockapi.io/logIn';
 
+        // Function to fetch and display images
+        async function fetchAndDisplayImages() {
+            const response = await fetch(apiUrl);
+            const images = await response.json();
+            imageContainer.innerHTML = '';
+            images.forEach(image => {
+                const imageDiv = document.createElement('div');
+                const img = document.createElement('img');
+                const name = document.createElement('h4');
+                const deleteBtn = document.createElement('button');
 
-let userName = document.getElementById('userName');
-let image1 = document.getElementById('image1');
-let image2 = document.getElementById('image2');
-let password = document.getElementById('password');
-let btn = document.getElementById('btn');
+                img.src = image.image2;
+                name.innerText = `Image Name: ${image.userName}`;
+                deleteBtn.innerText = 'Delete';
+                deleteBtn.onclick = () => deleteImage(image.id, imageDiv);
 
-
-console.log(userName);
-console.log(password);
-console.log(btn);
-
-
-async function logIn(){
-    let url = 'https://665736849f970b3b36c864e7.mockapi.io/logIn';
-    let response = await fetch(
-        url, {
-        method: 'POST',
-        body: JSON.stringify({
-            userName: userName.value,
-            password:  password.value,
-            // image1: image1.value,
-            image2: image2.value,
-
-          }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-
-    let data = await response.json();
-    let yourName = document.getElementById('y-name');
-    yourName.innerText = `your name: ${data.userName}`;
-    image1.src = image2.value;
-    console.log(data);
-    localStorage.setItem('userName', data.userName);
-
-
-    
-    for(let i = 0; i < data.length; i++){
-        let yourName = document.createElement('h4');
-        let image11 = document.createElement('img');
-
-        yourName.innerText = `your name: ${data[i].userName}`;
-        image11.src = `${data[i].image2}`;
-  
-    }
-}
-
-
-
-
-
-
-
-
-
-  let url = 'https://665736849f970b3b36c864e7.mockapi.io/logIn';
-
-  async function logOut(){
-        let data1 = await fetch(url);
-        let data2 = await data1.json();
-    
-        for(let i = 0; i < data2.length; i++){
-            let yourName1 = document.createElement('h4');
-            let image111 = document.createElement('img');
-            let btn1 = document.createElement('button');
-            btn1.className = 'btn1';
-            btn1.innerText = 'Delete';
-            btn1.addEventListener('click', deleteData);
-            yourName1.innerText = `your name: ${data2[i].userName}`;
-            image111.src = data2[i].image2; 
-            document.body.append(yourName1);
-            document.body.append(image111);
-            document.body.append(btn1);
-
+                imageDiv.appendChild(name);
+                imageDiv.appendChild(img);
+                imageDiv.appendChild(deleteBtn);
+                imageContainer.appendChild(imageDiv);
+            });
         }
 
-    }
+        // Function to add a new image
+        async function addImage() {
+            const newImage = {
+                userName: imageNameInput.value,
+                image2: imageUrlInput.value
+            };
+            await fetch(apiUrl, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(newImage)
+            });
+            fetchAndDisplayImages();
+        }
 
-    logOut()
+        // Function to delete an image
+        async function deleteImage(id, imageDiv) {
+            await fetch(`${apiUrl}/${id}`, {
+                method: 'DELETE'
+            });
+            imageContainer.removeChild(imageDiv);
+        }
 
+        // Event listeners
+        addImageBtn.addEventListener('click', addImage);
 
-    let url2 = 'https://665736849f970b3b36c864e7.mockapi.io/logIn';
-
-
-
-    const myDataObject = { id: 1 };
-
-    const deleteData = async ( ) =>{
-       const response = await fetch(url2, {
-           method: 'DELETE', 
-           headers: {
-             'Content-Type': 'application/json'
-           },
-           body: JSON.stringify(myDataObject)
-       });
-       
-      const data = await response.json( );
-      let btn1 = document.getElementById('btn1');
-
-
-       console.log(data);
-    };
-    deleteData( );
-
-
-
-
-
-
-
+        // Initial fetch and display
+        fetchAndDisplayImages();
 
 
 
