8658a4c27c2e1a808ce7664737899f44e59795c3
SARA3SAEED <Swaretoo@gmail.com>
2024-05-29 20:49:22 +0300

    delete  commit

--- main.js
+++ main.js
@@ -21,14 +21,10 @@ async function logIn(){
         url, {
         method: 'POST',
         body: JSON.stringify({
-           
             userName: userName.value,
             password:  password.value,
             // image1: image1.value,
             image2: image2.value,
-
-
-
 
           }),
         headers: {
@@ -39,9 +35,92 @@ async function logIn(){
     let data = await response.json();
     let yourName = document.getElementById('y-name');
     yourName.innerText = `your name: ${data.userName}`;
-    // image1.src = 'https://images.pexels.com/photos/57416/cat-sweet-kitty-animals-57416.jpeg?auto=compress&cs=tinysrgb&w=600';
     image1.src = image2.value;
     console.log(data);
     localStorage.setItem('userName', data.userName);
 
+
+    
+    for(let i = 0; i < data.length; i++){
+        let yourName = document.createElement('h4');
+        let image11 = document.createElement('img');
+
+        yourName.innerText = `your name: ${data[i].userName}`;
+        image11.src = `${data[i].image2}`;
+  
+    }
 }
+
+
+
+
+
+
+
+
+
+  let url = 'https://665736849f970b3b36c864e7.mockapi.io/logIn';
+
+  async function logOut(){
+        let data1 = await fetch(url);
+        let data2 = await data1.json();
+    
+        for(let i = 0; i < data2.length; i++){
+            let yourName1 = document.createElement('h4');
+            let image111 = document.createElement('img');
+            let btn1 = document.createElement('button');
+            btn1.className = 'btn1';
+            btn1.innerText = 'Delete';
+            btn1.addEventListener('click', deleteData);
+            yourName1.innerText = `your name: ${data2[i].userName}`;
+            image111.src = data2[i].image2; 
+            document.body.append(yourName1);
+            document.body.append(image111);
+            document.body.append(btn1);
+
+        }
+
+    }
+
+    logOut()
+
+
+    let url2 = 'https://665736849f970b3b36c864e7.mockapi.io/logIn';
+
+
+
+    const myDataObject = { id: 1 };
+
+    const deleteData = async ( ) =>{
+       const response = await fetch(url2, {
+           method: 'DELETE', 
+           headers: {
+             'Content-Type': 'application/json'
+           },
+           body: JSON.stringify(myDataObject)
+       });
+       
+      const data = await response.json( );
+      let btn1 = document.getElementById('btn1');
+
+
+       console.log(data);
+    };
+    deleteData( );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
